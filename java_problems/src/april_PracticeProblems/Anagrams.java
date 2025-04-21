8088092dfa8177d0f67566cbbbb2e31bcf51967b
SIVA <SIVA@LAPTOP-JHP6OGIG>
2025-04-22 05:05:54 +0530

    initial commit

--- java_problems/src/april_PracticeProblems/Anagrams.java
+++ java_problems/src/april_PracticeProblems/Anagrams.java
@@ -13,8 +13,10 @@ public class Anagrams {
 		char[] charArray1 = str1.toCharArray();
 		char[] charArray2 = str2.toCharArray();
 		
+		//sorting array
 		Arrays.sort(charArray1);
 		Arrays.sort(charArray2);
+		
 		
 		System.out.println(Arrays.equals(charArray1, charArray2));
 	}

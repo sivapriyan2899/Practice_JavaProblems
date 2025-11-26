3d556679854da8efb37ef6f58c1c3f481b1e6dd8
siva0 <sivapriyan2899@gmail.com>
2025-11-26 15:08:50 +0530

    sorting objects - solved

---
+++ java_problems/src/nov_problems_practice/SortingObjects.java
@@ -0,0 +1,45 @@
+package nov_problems_practice;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+public class SortingObjects {
+	
+	String name;
+	int age, salary;
+	
+	SortingObjects(String nameOfEmployee, int ageOfEmployee, int salaryOfEmployee) {
+	name=nameOfEmployee;
+	age=ageOfEmployee;
+	salary=salaryOfEmployee;
+	
+	}
+		
+	public static void main(String[] args) {
+		
+		List<SortingObjects> arrayList = new ArrayList<SortingObjects>();
+		
+		
+		  arrayList.add(new SortingObjects("siva",20,1000)); 
+		  arrayList.add(new SortingObjects("jon",25,6000)); 
+		  arrayList.add(new SortingObjects("remi",28,3000));
+		 
+		//arrayList.sort(Comparator.comparing(e-> e.age));
+		  
+		  arrayList.sort(new Comparator<SortingObjects>() {
+
+			@Override
+			public int compare(SortingObjects o1, SortingObjects o2) {
+				
+				return o2.age - o1.age;
+			}
+			  
+			  
+		});
+		
+		for(SortingObjects result : arrayList)
+		System.out.println(result.name +" "+result.age+" "+result.salary);
+		
+	}
+}
